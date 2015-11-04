3f8a96610775a0e2648e063502b0cc2512f61cad
jolive1993 <rockitshipshawty@gmail.com>
2015-11-04 12:06:08 -0600

    accident edit

--- tweep.py
+++ tweep.py
@@ -3,4 +3,5 @@ from keys import *
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
-print(api.trends_place(2379574))
+trend = (api.trends_place(2379574))
+print(trend)
