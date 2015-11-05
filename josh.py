36e6e6313727b3f3a6ae963bb4a87d450d25b208
jolive1993 <rockitshipshawty@gmail.com>
2015-11-05 17:25:39 -0600

    Added tweeter and tweet replyer

--- josh.py
+++ josh.py
@@ -32,7 +32,7 @@ class Follower:
         messageRecp = self.followers[random.randint(0,numFollowers)]
         api.send_direct_message(user_id = messageRecp, text = "hi")
         self.followers = []
-class RetweeterReplyer:
+class Retweeter:
     def __init__(self):
         self.tweetList = []
     def retweetdump(self):
@@ -66,20 +66,51 @@ class RetweeterReplyer:
         else:
             self.retweetFriends()
     def replyer(self):
-        self.searchQuery = "#PURPOSE"
+        searchJunk = "#Purpose"
+        self.searchQuery = searchJunk
         for result in api.search(q=self.searchQuery, lang="en"):
             self.tweetList.append(result.id)
+        time.sleep(2)
         numTweets = len(self.tweetList)
         rand = random.randint(0,numTweets)
-        print(self.tweetList[rand])
-        api.update_status("I belieb", in_reply_to_status_id=(self.tweetList[rand]))
+        tweetId = self.tweetList[rand]
+        user = api.get_status(tweetId)
+        time.sleep(2)
+        sn = user.user.screen_name
+        m = "@{0}, {1} {2}".format(sn, "biebs is my hero", searchJunk)
+        api.update_status(status=m, in_reply_to_status_id = tweetId)
         self.tweetList = []
+class TweetBot:
+    def __init__(self):
+        self.tweetList1 = []
+    def replyer(self):
+        searchJunk = "#Purpose"
+        self.searchQuery = searchJunk
+        for result in api.search(q=self.searchQuery, lang="en"):
+            self.tweetList1.append(result.id)
+        time.sleep(2)
+        numTweets = len(self.tweetList1)
+        rand = random.randint(0,numTweets)
+        tweetId = self.tweetList1[rand]
+        user = api.get_status(tweetId)
+        time.sleep(2)
+        sn = user.user.screen_name
+        content = "@{0}, {1} {2}".format(sn, "biebs is my hero", searchJunk)
+        api.update_status(status=content, in_reply_to_status_id = tweetId)
+        self.tweetList1 = []
+    def tweeter(self):
+        hashtag = "#bruh"
+        tweetMessage = "do you guys love biebs "
+        content = (tweetMessage + hashtag)
+        print(content)
+        api.update_status(status=content)
+        
+    
         
         
-ourAccount = Follower('beliebthehype')
-justinBieber = Follower('justinbieber')
-blah = RetweeterReplyer()
-blah.replyer()
+
+blah = TweetBot()
+blah.tweeter()
 
 
 
