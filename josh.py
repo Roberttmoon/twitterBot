506470116e7d59fa0098c0141940e7cd315eb656
jolive1993 <rockitshipshawty@gmail.com>
2015-11-05 10:31:06 -0600

    auto follower only follows one user

--- josh.py
+++ josh.py
@@ -4,9 +4,11 @@ import tweepy, sys, os
 import time
 from keys import *
 import random
+import json
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
+dump = open('dump.txt', 'a+')
 def uprint(*objects, sep=' ', end='\n', file=sys.stdout):
     enc = file.encoding
     if enc == 'UTF-8':
@@ -20,11 +22,11 @@ class FollowerGenerator:
     def followerFinder(self):
         self.followers = api.followers_ids(self.userName)
     def follower(self):
-        counter = 0
-        for x in self.followers:
+        counter = random.randint(0,100)
+        for x in range(1):
             api.create_friendship(self.followers[counter])
             print("followed " + str(self.followers[counter]))
-            counter += 1
+            counter -= 1
     def followerGenerator():
         followDude = FollowerGenerator()
         followDude.followerFinder()
@@ -33,6 +35,11 @@ class Retweeter:
     def __init__(self):
         self.dumptruck = []
         self.searchQuery = str(input("Enter Search Term for retweet"))
+    def retweetdump(self):
+        rand = random.randint(0,10)
+        for result in api.search(q=self.searchQuery, lang="en"):
+            self.dumptruck.append(result.id)
+        json.dump([self.dumptruck], dump)
     def retweet(self):
         rand = random.randint(0,10)
         for result in api.search(q=self.searchQuery, lang="en"):
@@ -41,5 +48,7 @@ class Retweeter:
     def retweeter():
         retweetDude = Retweeter()
         retweetDude.retweet()
+FollowerGenerator.followerGenerator()
+
 
     
