69509a78ba767d301bd80d7f4cd38129fcd890e7
Robert Moon <roberttmoon@gmail.com>
2015-11-06 09:34:19 -0600

    Commiting working verson of core, tested.

--- core.py
+++ core.py
@@ -28,7 +28,7 @@ class Scan_Trends:
         trend_hashes= hashtags[0]
         #tweetSearch_results= api.search(q=trend_hashes,count= 50)
 
-        return hashTags
+        print(hashtags)
 
 class Follower:
     def __init__(self, twitterHandle):
@@ -42,7 +42,7 @@ class Follower:
             counter -= 1
     def messager(self):
         numFollowers = len(self.followers)
-        messageRecp = self.followers[random.randint(0,numFollowers)]
+        messageRecp = self.followers[randint(0,numFollowers-1)]
         msg = strings.replys[randint(0,len(strings.replys)-1)]
         api.send_direct_message(user_id = messageRecp, text = msg)
 
@@ -51,7 +51,7 @@ class Retweeter:
         self.tweetList = []
     def retweetdump(self):
         self.searchQuery = strings.hashTags[randint(0,len(strings.hashTags)-1)]
-        rand = random.randint(0,10)
+        rand = randint(0,10)
         for result in api.search(q=self.searchQuery, lang="en"):
             self.tweetList.append(result.id)
         json.dump([self.tweetList], dump)
@@ -60,22 +60,25 @@ class Retweeter:
         for result in api.search(q=self.searchQuery, lang="en"):
             self.tweetList.append(result.id)
         numTweets = len(self.tweetList)
-        rand = random.randint(0,numTweets)
+        rand = randint(0,numTweets)
         api.retweet(self.tweetList[rand])
     def retweetFriends(self):
-        numFollowers = len(ourAccount.followers)
-        messageRecp = ourAccount.followers[random.randint(0,numFollowers)]
+        ourAccount = Follower("beliebthehype")
+        numFollowers = len(ourAccount.followers)-1
+        messageRecp = ourAccount.followers[randint(0,numFollowers)]
         for result in api.user_timeline(messageRecp):
             self.tweetList.append(result.id)
         numTweets = len(self.tweetList)
-        rand = random.randint(0,numTweets)
+        rand = randint(0,numTweets-1)
         api.retweet(self.tweetList[rand])
     def retweeterRandom(self):
-        rand = random.randint(0,1)
-        if rand == 0:
+        rand = randint(0,1)
+        if rand > 0:
             self.retweetSearch()
+            print("retweet hashtag search")
         else:
             self.retweetFriends()
+            print("retweet follower")
 
 class TweetBot:
     def __init__(self):
@@ -87,19 +90,20 @@ class TweetBot:
             self.tweetList1.append(result.id)
         time.sleep(2)
         numTweets = len(self.tweetList1)
-        rand = random.randint(0,numTweets)
+        rand = randint(0,numTweets)
         tweetId = self.tweetList1[rand]
         user = api.get_status(tweetId)
         time.sleep(2)
         sn = user.user.screen_name
         msg = strings.replys[randint(0,len(strings.replys)-1)]
         content = "@{0}, {1} {2}".format(sn, msg, searchJunk)
+        print(content)
         api.update_status(status=content, in_reply_to_status_id = tweetId)
         self.tweetList1 = []
     def tweeter(self):
         hashtag = strings.hashTags[randint(0,len(strings.hashTags)-1)]
         tweetMessage = strings.replys[randint(0,len(strings.replys)-1)]
-        content = (tweetMessage + hashtag)
+        content = (tweetMessage, hashtag)
         print(content)
         api.update_status(status=content)
     def tweetChooser(self):
@@ -115,36 +119,41 @@ class Core:
     def core(self):
         retweeter = Retweeter()
         scanTrends = Scan_Trends()
-        justinFollower = Follower("justinbieber")
-        userFollower = Follower("beliebthehype")
+        justinbieber = Follower("justinbieber") #change this to target account
+        ourAccount = Follower("beliebthehype") #change this to self account
+        print(ourAccount.followers)
         tweetBot = TweetBot()
         print("starting?")
         while True:
             sleepy = randint(14400,21600)
             resty = randint(60,300)
-            #the 15 mins twitter needs for clearing 429error
-            resetTime = 900 
+            resetTime = 900 #the 15 mins twitter needs for clearing 429error 
             probs = randint(1,1000)
             if probs < 100:
                 #tweet canned message
                 print("canned message")
                 tweetBot.tweetChooser()
+                print("sleeping for", resty)
                 time.sleep(resty)
             elif probs < 300:
                 #look for followers
                 try:
                     print('trying for followers')
-                    #uncomment the one you want to use!
-                    #justinFollower.follower()
-                    #userFollower.follower()
+                    if probs < 200:
+                        print("following a follower of JB") 
+                        justinbieber.follower()
+                    else:
+                        print("following a follower of us")
+                        ourAccount.follower()
                 except:
                     print("timed out, passing")
                     time.sleep(resetTime)
                     pass
                 print("look for followers")
+                print("sleeping for", resty)
                 time.sleep(resty)
             elif probs < 400:
-                #look to respond to top trending beiberTag
+                #look for to top trending beiberTag
                 try:
                     print("scanning trends")
                     scanTrends.scanner()
@@ -153,6 +162,7 @@ class Core:
                     time.sleep(resetTime)
                     pass
                 print("top trending")
+                print("sleeping for", resty)
                 time.sleep(resty)
             elif probs < 900:
                 #retweet a beiber related tweet
@@ -162,10 +172,12 @@ class Core:
                 except:
                     print("retweet failed, passing")
                     time.sleep(resetTime)
+                print("sleeping for", resty)
                 time.sleep(resty)
             else:
                 #go to bed
-                print("sleep")
+                print("sleep for",sleepy)
+                time.sleep(sleepy)
 
 main = Core()
 main.core()
